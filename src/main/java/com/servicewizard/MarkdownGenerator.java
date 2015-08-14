b64db0975e80a5653e6ae1fc6e707f9580df61d9
jcbauschatz <jcbauschatz@revolutionehr.com>
2015-08-14 13:16:01 -0500

    Changed to stream-based output so that files can be writte

--- src/main/java/com/servicewizard/MarkdownGenerator.java
+++ src/main/java/com/servicewizard/MarkdownGenerator.java
@@ -10,13 +10,32 @@ package com.servicewizard;
 import com.servicewizard.model.Service;
 import com.servicewizard.model.ServiceMethod;
 
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.List;
 
 public class MarkdownGenerator {
 
 	public void generate(List<Service> services) {
+		try {
+			generate(services, System.out);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void generate(List<Service> services, String fileName) {
+		try {
+			PrintStream fileWriter = new PrintStream(fileName);
+			generate(services, fileWriter);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void generate(List<Service> services, PrintStream output) throws IOException {
 		for (Service service : services) {
-			System.out.println("# " + service.getName());
+			output.println("# " + service.getName());
 
 			for (ServiceMethod method : service.getMethods()) {
 				String verbName = "[" + method.getVerb() + "]";
@@ -31,9 +50,9 @@ public class MarkdownGenerator {
 					paramString += ")";
 				}
 
-				System.out.println(String.format("## %-8s %s%s", verbName, method.getRelativePath(), paramString));
+				output.println(String.format("## %-8s %s%s", verbName, method.getRelativePath(), paramString));
 			}
-			System.out.println();
+			output.println();
 		}
 	}
 }
