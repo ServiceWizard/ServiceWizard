159432fec36f71f6815e15a3cae8cb1a2b8cf93f
forana <alex@catalyze.io>
2015-09-05 15:40:56 -0500

    dropwizard to latest stable

--- src/sample/java/com/sampleapp/ToDoAPI.java
+++ src/sample/java/com/sampleapp/ToDoAPI.java
@@ -2,13 +2,6 @@ package com.sampleapp;
 
 import com.sampleapp.resource.ToDoItemService;
 
-import org.eclipse.jetty.servlets.CrossOriginFilter;
-
-import java.util.EnumSet;
-
-import javax.servlet.DispatcherType;
-import javax.servlet.FilterRegistration;
-
 import io.dropwizard.Application;
 import io.dropwizard.Configuration;
 import io.dropwizard.setup.Bootstrap;
@@ -26,14 +19,5 @@ public class ToDoAPI extends Application<Configuration> {
     @Override
     public void run(Configuration configuration, Environment environment) throws Exception {
         environment.jersey().register(new ToDoItemService());
-
-        // CORS support
-        FilterRegistration.Dynamic filter = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
-        filter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
-        filter.setInitParameter("allowedOrigins", "*");
-        filter.setInitParameter("allowedHeaders", "Authorization,Content-Type,X-Api-Key,Accept,Origin");
-        filter.setInitParameter("allowedMethods", "GET,POST,PUT,DELETE,OPTIONS");
-        filter.setInitParameter("preflightMaxAge", "5184000"); // 2 months
-        filter.setInitParameter("allowCredentials", "true");
     }
 }
